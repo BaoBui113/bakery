6037b578535f0ce64cfd5a971ed99261d9a051d6
baobytesquad <giabao@bytesquad.tech>
2025-05-12 00:08:53 +0700

    update main

---
+++ src/services/admin/report/index.tsx
@@ -0,0 +1,21 @@
+import axiosInstance from "@/helper/axios";
+import { buildQueryString } from "@/helper/buildQueryString";
+import { QueryFunctionContext } from "@tanstack/react-query";
+
+// export const getReport = async (type: string) => {
+//   const res = await axiosInstance.get(`/reports?type=${type}`);
+//   return res.data;
+// };
+
+export const getReport = async ({
+  queryKey,
+}: QueryFunctionContext<[string, string]>) => {
+  const [, search] = queryKey;
+  console.log("search", search);
+
+  const queryString = buildQueryString(JSON.parse(search));
+  console.log("queryString", queryString);
+
+  const res = await axiosInstance.get(`/reports?${queryString}`);
+  return res.data;
+};
